68999f7990694183d2088461afa5188e85a543f8
Mounir Ez-zahar <mounirzhar@gmail.com>
2018-03-15 16:20:16 +0100

    Fixed readPermission evaluation and added PostFilter to project entopics

--- src/main/java/com/miage/spring/model/Project.java
+++ src/main/java/com/miage/spring/model/Project.java
@@ -30,15 +30,19 @@ public class Project extends HasPermissions implements Serializable {
 	private Long id;
 	@NotEmpty
 	private String name;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
