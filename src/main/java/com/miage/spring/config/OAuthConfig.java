7a77524a26c525b303c33842ee4d03162f47da22
Mounir Ez-zahar <mounirzhar@gmail.com>
2018-03-16 16:19:05 +0100

    Added a goal to only build tests

--- src/main/java/com/miage/spring/config/OAuthConfig.java
+++ src/main/java/com/miage/spring/config/OAuthConfig.java
@@ -7,6 +7,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.logging.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/miage/spring/service/implement/MailServiceImpl.java
+++ src/main/java/com/miage/spring/service/implement/MailServiceImpl.java
@@ -1,15 +1,16 @@
 package com.miage.spring.service.implement;
 
-
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.miage.spring.model.Post;
 import com.miage.spring.model.Topic;
@@ -18,17 +19,18 @@ import com.miage.spring.repository.TopicRepository;
 import com.miage.spring.service.MailService;
 
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Service
 public class MailServiceImpl implements MailService {
-	
+
 	@Autowired
-	private TopicRepository topicRepository ;
+	private TopicRepository topicRepository;
 	@Autowired
-	private JavaMailSender sender ;
+	private JavaMailSender sender;
 	@Autowired
-	private TemplateEngine templateEngine ;
-	
+	private TemplateEngine templateEngine;
+
 	@Override
 	@Transactional(readOnly = true)
 	public void sendNotifToAllFollowers(Post post) {
@@ -39,10 +41,11 @@ public class MailServiceImpl implements MailService {
 			followers.forEach(follower -> sendNotifEmail(follower, author, topic, post));
 		}
 	}
+
 	/**
 	 * Method that send an email to a user, about a new post in a topic
 	 */
-	
+
 	/**
 	 * 
 	 * @param user
@@ -50,14 +53,32 @@ public class MailServiceImpl implements MailService {
 	 * @param topic
 	 * @param post
 	 */
-	private void sendNotifEmail(User user, User author,Topic topic,Post post) {
-		//Don't send email to creator
+	private void sendNotifEmail(User user, User author, Topic topic, Post post) {
+		// Don't send email to creator
 		if (user.getEmail().equals(author.getEmail())) {
 			return;
 		}
-		MimeMessagePreparator messagePreparator= mimeMessage ->{
+		MimeMessagePreparator messagePreparator = mimeMessage -> {
 			MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+			messageHelper.setFrom("do-not-reply@spring-forum.com");
+			messageHelper.setTo(user.getEmail());
+			messageHelper.setSubject("New message in topic:" + topic.getTitle());
+			String content = buildMailTemplate(user.getFirstname(), author.getUsername(), post.getContent());
+			messageHelper.setText(content, true);
+		};
+		try {
+			sender.send(messagePreparator);
+		} catch (MailException e) {
+			log.error(e.getMessage(), e);
 		}
+	}
+
+	private String buildMailTemplate(String firstname, String author, String content) {
+		Context context = new Context();
+		context.setVariable("firstName", firstname);
+		context.setVariable("content", content);
+		context.setVariable("author", author);
+		return templateEngine.process("mail/notification", context);
 	}
 
 }
