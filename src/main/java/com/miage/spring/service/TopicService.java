0043ec2a099b4b309930b5375561a0df93f98d3f
Mounir Ez-zahar <mounirzhar@gmail.com>
2018-03-26 16:36:15 +0100

    Refactoring and optimisations

--- src/main/java/com/miage/spring/service/TopicService.java
+++ src/main/java/com/miage/spring/service/TopicService.java
@@ -2,6 +2,7 @@ package com.miage.spring.service;
 
 import java.util.List;
 
+import com.miage.spring.exception.ProjectNotFoundException;
 import com.miage.spring.model.Project;
 import com.miage.spring.model.Topic;
 import com.miage.spring.model.User;
@@ -9,7 +10,7 @@ import com.miage.spring.model.User;
 public interface TopicService {
 	Topic save(Topic topic);
 
-	//Topic save(Topic topic, Long projectId) throws ProjectNotFoundException;
+	Topic save(Topic topic, Long projectId) throws ProjectNotFoundException;
 
 	Topic getOne(Long id);
 
