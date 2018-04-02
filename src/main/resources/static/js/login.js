89c51cf5737e546f940ebe29c76eee92db26fc49
Mounir Ez-zahar <mounirzhar@gmail.com>
2018-04-02 11:18:31 +0100

    Added Simple load tests

--- src/main/resources/static/js/login.js
+++
@@ -1,9 +0,0 @@
-(()=>{
-	const = loginBtn = document.getElementById('loginBtn')
-	const = loginForm = document.getElementById('loginForm')
-	
-	loginBtn.addEventListener('click', () => {
-		loginForm.submit()
-	})
-})()
-
